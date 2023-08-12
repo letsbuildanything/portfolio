c8c4429ca04de3ed2eceaae5ac716c8bcce58468
letsbuildanything <impankaj1721@gmail.com>
2023-08-12 15:40:26 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,28 @@
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "@/components";
+import Image from "next/image";
+
+const App = () => {
+
+  // console.log(bgImage)
+  return (
+    <div className="relative z-0 bg-primary" >
+      <div className='relative'>
+        <Image src='/../public/assets/herobg.png' fill={true} style = {{ objectFit:'cover' }} alt='hero-backround-image' />
+        <Navbar />
+        <Hero />
+        
+      </div>
+      <About />
+      <Experience />
+      <Tech />
+      <Works />
+      <Feedbacks />
+      <div className='relative z-0'>
+        <Contact />
+        <StarsCanvas />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+import {logo} from '@/public/assets';
+
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    '../public/**/*.{png,jpg,svg',
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'hero_pattern': `url('/../public/assets/herobg.png')`,
+      },
+      colors: {
+        primary: "#050816",
+        secondary: "#aaa6c3",
+        tertiary: "#151030",
+        "black-100": "#100d25",
+        "black-200": "#090325",
+        "white-100": "#f3f3f3",
+      },
+      boxShadow: {
+        card: "0px 35px 120px -15px #211e35",
+      },
+      screens: {
+        xs: "450px",
+      },
+    },
+  },
+  plugins: [],
+}
